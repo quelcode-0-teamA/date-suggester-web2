a3d85b038ab14e2cbb055cd54e7d0919964d92fa
yuta-san765 <nitoplus765@gmail.com>
2020-03-24 15:19:00 +0900

    Install sass-loader, and Created TheHeader

---
+++ components/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <h1 class="header__title">Date Suggester</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.header {
+  background-color: #de436a;
+  height: 57px;
+  text-align: center;
+  &__title {
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',
+      sans-serif;
+    font-size: 23px;
+    color: white;
+    line-height: 57px;
+    font-weight: 400;
+    font-style: italic;
+  }
+}
+</style>
