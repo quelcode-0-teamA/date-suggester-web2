385e3e93f66a31c81b7a48107e827e7cb31f7e3a
yuta-san765 <nitoplus765@gmail.com>
2020-04-02 14:11:52 +0900

    asyncData works

--- pages/suggest.vue
+++ pages/suggest.vue
@@ -3,7 +3,27 @@
 </template>
 
 <script>
-export default {}
+export default {
+  asyncData({ store, $axios, app }) {
+    const dateToken = app.$cookies.get('dstoken')
+    $axios
+      .$get(`${store.getters.qParams}`, {
+        headers: {
+          Authorization: 'Bearer ' + dateToken
+        }
+      })
+      .then((response) => {
+        console.log(response)
+        return {
+          dateToken,
+          suggest: response
+        }
+      })
+  },
+  data() {
+    return {}
+  }
+}
 </script>
 
 <style></style>
