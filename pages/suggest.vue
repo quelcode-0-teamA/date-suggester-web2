f9e1a48852a258a52ac854f605e1fd3634c9d1ee
yuta-san765 <nitoplus765@gmail.com>
2020-04-03 14:43:34 +0900

    Switched to suggest page

--- pages/suggest.vue
+++ pages/suggest.vue
@@ -1,24 +1,35 @@
 <template>
   <div>
-    <h2>{{ suggest }}</h2>
-    <!-- <div class="headline-container">
-      <div>
-        <h2>{{ suggest.title }}</h2>
-        <p>{{ suggest.description }}</p>
-        <span>
-          <font-awesome-icon icon="yen-sign"></font-awesome-icon>
-          {{ suggest.total_budget }} 円
-        </span>
-        <span>
-          <font-awesome-icon icon="map-marker-alt"></font-awesome-icon>
-          {{ suggest.area }}
-        </span>
+    <div class="headline-container">
+      <div class="headline-container__text">
+        <h2 class="headline-container__title">{{ suggest.title }}</h2>
+        <p class="headline-container__description">{{ suggest.description }}</p>
+        <div class="headline-container__info">
+          <span class="headline-container__info-text">
+            <font-awesome-icon
+              class="headline-container__icon"
+              icon="yen-sign"
+            ></font-awesome-icon>
+            {{ suggest.total_budget }}
+          </span>
+          <span>
+            <font-awesome-icon
+              class="headline-container__icon"
+              icon="map-marker-alt"
+            ></font-awesome-icon>
+            {{ suggest.area }}
+          </span>
+        </div>
       </div>
       <div>
-        <img :src="suggest.thumb" alt="デートのメインコンテンツの画像です" />
+        <img
+          class="headline-container__photo"
+          :src="suggest.thumb"
+          alt="デートのメインコンテンツの画像です"
+        />
       </div>
     </div>
-    <div class="spot-table"></div> -->
+    <div class="spots-container"></div>
   </div>
 </template>
 
@@ -26,7 +37,7 @@
 export default {
   asyncData({ store, $axios, app }) {
     const dateToken = app.$cookies.get('dstoken')
-    $axios
+    return $axios
       .$get(`/v1/plans/suggest?${store.getters.qParams}`, {
         headers: {
           Authorization: 'Bearer ' + dateToken
@@ -51,5 +62,37 @@ export default {
   width: 720px;
   height: 240px;
   display: flex;
+  margin: 0 auto;
+  padding: 32px 0 24px;
+  position: relative;
+  &__text {
+    color: white;
+    padding-right: 70px;
+  }
+  &__title {
+    padding: 16px 0 22px;
+    font-size: 26px;
+  }
+  &__description {
+    font-size: 14px;
+  }
+  &__info {
+    position: absolute;
+    bottom: 24px;
+  }
+  &__icon {
+    opacity: 0.5;
+  }
+  &__info-text {
+    margin-right: 36px;
+  }
+  &__photo {
+    width: 312px;
+    height: 184px;
+  }
+}
+
+.spots-container {
+  background-color: #f5f5f5;
 }
 </style>
