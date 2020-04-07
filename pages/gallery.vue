4a257fd2265c6aae6c75f9fb73b48fe09cd76da0
yuta-san765 <nitoplus765@gmail.com>
2020-04-07 16:33:16 +0900

    gallery layout done.

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -2,12 +2,19 @@
   <div class="gallery">
     <h2 class="gallery__title">デートプラン一覧</h2>
     <div class="gallery__list">
-      <figure v-for="(item, index) in galleryItems" :key="index">
+      <figure
+        v-for="(item, index) in galleryItems"
+        :key="index"
+        class="gallery__figure"
+      >
         <img
+          class="gallery__img"
           :src="item.plan.thumb"
           alt="ギャラリー内のデートプランの項目タイトルです"
         />
-        <figcaption>{{ item.plan.title }}</figcaption>
+        <figcaption class="gallery__figcaption">
+          {{ item.plan.title }}
+        </figcaption>
       </figure>
     </div>
   </div>
@@ -37,15 +44,42 @@ export default {
 <style lang="scss">
 .gallery {
   background-color: #fff;
-  height: 100vh;
+  min-height: calc(100vh - 57px);
   &__title {
-    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 104px;
     color: #ff5492;
     font-size: 19px;
   }
   &__list {
     width: 720px;
     display: flex;
+    flex-wrap: wrap;
+    margin: 0 auto;
+    justify-content: space-between;
+  }
+  &__figure {
+    position: relative;
+    margin-bottom: 35px;
+  }
+  &__img {
+    width: 336px;
+    height: 232px;
+    object-fit: cover;
+  }
+  &__figcaption {
+    width: 100%;
+    height: 40px;
+    color: white;
+    font-size: 18px;
+    background-color: rgba($color: #ff5492, $alpha: 0.67);
+    position: absolute;
+    bottom: 6px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 }
 </style>
