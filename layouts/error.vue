a58d2ee111be2a2e001798edfec204df0f17d4a7
yuta-san765 <nitoplus765@gmail.com>
2020-04-28 16:28:02 +0900

    Create 404 error page

---
+++ layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h2>{{ error.StatusCode }}</h2>
+    <h3 v-if="error.StatusCode === 404">
+      ページが見つかりませんでした。
+      <nuxt-link to="/">
+        ホーム画面
+      </nuxt-link>
+      から再度お試しください。
+    </h3>
+    <h3 v-else>エラーが発生しました。</h3>
+    <nuxt-link class="error-link" to="/">ホーム画面に戻る</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.error-link {
+  text-decoration: underline;
+}
+</style>
